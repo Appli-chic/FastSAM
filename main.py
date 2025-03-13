4d82068b56673e12538c2fa6d8aaa7bd4b8d1cd7
Guillaume Belouin <gbelouin@protonmail.com>
2025-03-13 15:55:06 +0100

    Add output directory to .gitignore, update image output path in main.py, and include supervision package in requirements.txt

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from PIL import Image
+
 from fastsam import FastSAM, FastSAMPrompt
+import supervision as sv
 
 model = FastSAM('./weights/FastSAM-x.pt')
 IMAGE_PATH = 'images/dog.jpg'
+OUTPUT_IMAGE_PATH = 'outputs/dog.jpg'
 DEVICE = 'cuda'
+
 everything_results = model(IMAGE_PATH, device=DEVICE, retina_masks=True, imgsz=1024, conf=0.4, iou=0.7)
 prompt_process = FastSAMPrompt(IMAGE_PATH, everything_results, device=DEVICE)
 
 ann = prompt_process.everything_prompt()
 
-prompt_process.plot(annotations=ann,output_path='./output/output.jpg')
+prompt_process.plot(annotations=ann,output_path=OUTPUT_IMAGE_PATH)
+
+image = Image.open(OUTPUT_IMAGE_PATH)
+sv.plot_image(image)
