f9136dc54ea11503cb7b0417c7a91d117faa8c45
Guillaume Belouin <gbelouin@protonmail.com>
2025-03-13 16:00:12 +0100

    Update output image path in main.py to 'output/output.jpg'

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import supervision as sv
 
 model = FastSAM('./weights/FastSAM-x.pt')
 IMAGE_PATH = 'images/dog.jpg'
-OUTPUT_IMAGE_PATH = 'output/dog.jpg'
+OUTPUT_IMAGE_PATH = 'output/output.jpg'
 DEVICE = 'cuda'
 
 everything_results = model(IMAGE_PATH, device=DEVICE, retina_masks=True, imgsz=1024, conf=0.4, iou=0.7)
