98ef1a86f78ccae09abec92a77cc6f0b179269b8
Guillaume Belouin <gbelouin@protonmail.com>
2025-03-13 18:02:31 +0100

    Add command-line arguments for segmentation type and input text in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 from fastsam import FastSAM, FastSAMPrompt
 import supervision as sv
+import argparse
 
 model = FastSAM('./weights/FastSAM-x.pt')
 IMAGE_PATH = 'images/dog.jpg'
@@ -11,16 +12,19 @@ DEVICE = 'cuda'
 everything_results = model(IMAGE_PATH, device=DEVICE, retina_masks=True, imgsz=1024, conf=0.4, iou=0.7)
 prompt_process = FastSAMPrompt(IMAGE_PATH, everything_results, device=DEVICE)
 
-print("Que voulez-vous faire ?")
-print("1 - Tout segmenter")
-print("2 - Segmenter via un texte")
-user_input = input("Tapez 1 ou 2: ")
+# types
+# 1 - Tout segmenter
+# 2 - Segmenter via un texte
+# input - Texte pour la segmentation
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--type", type=int)
+parser.add_argument("-i", "--input", type=str)
+args = parser.parse_args()
 
-if user_input == '1':
+if args.type == 1:
     ann = prompt_process.everything_prompt()
 else:
-    user_input = input("Quel texte voulez-vous utiliser ? ")
-    ann = prompt_process.text_prompt(user_input)
+    ann = prompt_process.text_prompt(args.input)
 
 prompt_process.plot(annotations=ann,output_path=OUTPUT_IMAGE_PATH)
 
